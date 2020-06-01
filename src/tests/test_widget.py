8dfe4d2cee0811033fb97061ff25d10dec9974be
Grzegorz Bokota <bokota+github@gmail.com>
2020-06-01 16:15:10 +0200

    check register first

--- src/tests/test_widget.py
+++ src/tests/test_widget.py
@@ -4,5 +4,6 @@ from sample_qt_error import MyComboBox
 class TestMyComboBox:
     def test_simple(self, qtbot):
         box = MyComboBox()
-        box.show()
         qtbot.add_widget(box)
+        box.show()
+
